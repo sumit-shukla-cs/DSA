d7d5916666982fdaedff3d580cf20f564b4c70d1
sumit-shukla-cs <sumitshukla0067@gmail.com>
2026-02-03 23:42:57 +0530

    ArrayList

---
+++ ArrayList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        System.out.println(list);
+    }
+}
