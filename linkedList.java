8ad380a1bfe554f78cdc938b8d95b3c0592dd5b2
sumit-shukla-cs <sumitshukla0067@gmail.com>
2026-02-04 00:13:06 +0530

    linkedList and stack

---
+++ linkedList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class linkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+        System.out.println(linkedList);
+    }
+    
+}
---
+++ stack.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+    }
+    
+}
